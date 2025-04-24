a6adb3465f09ce57646b3984415c7f1af76bc530
Shashank Kalluri <skalluri123@hotmail.com>
2025-04-24 01:25:27 -0400

    speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -7,36 +7,40 @@ import TweetBoard from "@/components/TweetBoard";
 import { faqItems } from "@/data/faq";
 import Sponsors from "@/components/Sponsors";
 import ImageCloud from "@/components/ImageCloud";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   const eventDate = "2025-12-05T10:00:00";
 
   return (
-    <div>
-      <section id="">
-        <Hero />
-      </section>
-      <section id="">
-        <ImageCloud />
-      </section>
-      <section id="">
-        <Countdown eventDate={eventDate}></Countdown>
-      </section>
-      <section id="universities">
-        <ContentMap />
-      </section>
-      <section id="sponsors">
-        <Sponsors />
-      </section>
-      {/* <section id="speakers">
+    <>
+      <div>
+        <section id="">
+          <Hero />
+        </section>
+        <section id="">
+          <ImageCloud />
+        </section>
+        <section id="">
+          <Countdown eventDate={eventDate}></Countdown>
+        </section>
+        <section id="universities">
+          <ContentMap />
+        </section>
+        <section id="sponsors">
+          <Sponsors />
+        </section>
+        {/* <section id="speakers">
         <Speakers />
       </section> */}
-      <section id="">
-        <TweetBoard />
-      </section>
-      <section id="faq">
-        <FAQ items={faqItems} />
-      </section>
-    </div>
+        <section id="">
+          <TweetBoard />
+        </section>
+        <section id="faq">
+          <FAQ items={faqItems} />
+        </section>
+      </div>
+      <SpeedInsights />
+    </>
   );
 }
