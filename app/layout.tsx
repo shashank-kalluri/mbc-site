cedd93c4aebed55eecb1950aeeede52a56cac1d0
Shashank Kalluri <skalluri123@hotmail.com>
2025-04-24 01:48:00 -0400

    sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
     apple: "/MBC Logo-08-white.png",
   },
   openGraph: {
-    siteName: "https://mbc-site-five.vercel.app/",
+    siteName: "https://midwestblockchain.org/",
     images: [
       {
         url: "https://raw.githubusercontent.com/shashank-kalluri/mbc-site/refs/heads/main/public/MBC%20Logo-02.png", // Must be an absolute URL
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "https://mbc-site-five.vercel.app/",
+    title: "https://midwestblockchain.org/",
     description: "The student blockchain conference",
     creator: "@MBC_Conference",
     creatorId: "1805686542125744129",
