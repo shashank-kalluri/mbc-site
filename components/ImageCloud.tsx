e0aad8f51913e153178dcb86fbff73e8c64d9884
Shashank Kalluri <skalluri123@hotmail.com>
2025-07-20 14:49:41 -0400

    speaker link new tab

--- components/ImageCloud.tsx
+++ components/ImageCloud.tsx
@@ -144,7 +144,13 @@ export default function ImageCloud() {
         </p>
         <div className="flex flex-wrap sm:flex-row gap-4">
           <Button variant="default">
-            <a href="https://forms.gle/xaPMhXr5EaJhYaut8">Apply to Speak</a>
+            <a
+              href="https://forms.gle/xaPMhXr5EaJhYaut8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Apply to Speak
+            </a>
           </Button>
           <Button asChild variant="secondary">
             <a
