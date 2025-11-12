ad620f579f81a457dd58a837020a40f610aa54e6
Shashank Kalluri <skalluri123@hotmail.com>
2025-11-12 15:41:31 +0000

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: "https://midwestblockchain.org/programs",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
   ];
 }
